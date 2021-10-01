2aef00f5401ea6d13e410ee19968db32ac032b3f
Malcolm Vigren <trekommafem2@gmail.com>
2021-10-01 16:34:18 +0200

    Start working on transformation of targets

--- python/classification/datasetgenerator.py
+++ python/classification/datasetgenerator.py
@@ -15,7 +15,11 @@ TARGET_EDGE_SIZE = 20
 
 
 def main():
-    _plot_targets()
+    #_plot_targets()
+    #generate_dataset()
+
+    targets = get_targets()
+    _random_perspective_distortion(targets['A'])
 
 
 def _plot_targets():
@@ -31,14 +35,13 @@ def _plot_targets():
 
 def generate_dataset():
     images = read_all_images()
+    targets = get_targets()
 
     plt.figure(1)
     while True:
-        patch = get_random_image_patch(images)
+        patch = generate_random_ground_truth_image(targets, images)
         plt.imshow(patch)
         plt.pause(1)
-        plt.show()
-        break
 
 
 def get_random_image_patch(images):
@@ -53,6 +56,59 @@ def get_random_image_patch(images):
     patch = image[y:y + ph, x:x + pw, :]
 
     return np.flip(patch, axis=-1)
+
+
+def generate_random_ground_truth_image(targets, background_images):
+    pass
+
+
+def randomly_distort_target_image(target_image):
+    pass
+
+
+def _random_perspective_distortion(target_image):
+    plt.figure(1)
+    #plt.subplot(4, 4, 1)
+
+    while True:
+        h, w, _ = target_image.shape
+
+        g = float(random.randint(0, 1000) - 500) / 5e5
+        h = float(random.randint(0, 1000)- 500) / 5e5
+
+
+        translation1 = np.array([
+            [1., 0., -h/2],
+            [0., 1., -w/2],
+            [0., 0., 1.],
+        ])
+
+        matrix = np.array([
+            [1., 0., 0.],
+            [0., 1., 0.],
+            [h, g, 1.],
+        ])
+
+        translation2 = np.array([
+            [1., 0., h/2],
+            [0., 1., w/2],
+            [0., 0., 1.],
+        ])
+
+        total_transformation = np.matmul(np.matmul(translation1, matrix), translation2)
+        total_transformation = total_transformation / total_transformation[2, 2]
+        #warped1 = cv2.warpPerspective(target_image, matrix, target_image.shape[:2])
+
+        #plt.subplot(1, 2, 1)
+        #plt.imshow(warped1)
+
+        warped2 = cv2.warpPerspective(target_image, total_transformation, target_image.shape[:2])
+        #plt.subplot(1, 2, 2)
+        plt.imshow(warped2)
+        plt.title(f"h={h:.5f}\ng={g:.5f}")
+
+        plt.pause(1)
+        plt.clf()
 
 
 def get_targets():
@@ -75,7 +131,8 @@ def get_targets():
             if x in (0, num_squares_w - 1) or y in (0, num_squares_h - 1):
                 # this will create alternating squares
                 color = float((y % 2) ^ (x % 2))
-                target_image[y*TARGET_EDGE_SIZE:(y + 1)*TARGET_EDGE_SIZE, x*TARGET_EDGE_SIZE:(x + 1)*TARGET_EDGE_SIZE, :] = color
+                target_image[y*TARGET_EDGE_SIZE:(y + 1)*TARGET_EDGE_SIZE,
+                             x*TARGET_EDGE_SIZE:(x + 1)*TARGET_EDGE_SIZE, :] = color
 
     target_images = {}
     for label, template in target_templates.items():
@@ -102,4 +159,7 @@ def create_checker_corner(image, y, x, h, w):
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
